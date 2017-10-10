d38bd59439f752f9d1964b33fa558a8be4659881
Tim Reilly <timreilly@live.com>
2017-10-10 15:51:32 -0700

    practice

--- app.js
+++ app.js
@@ -15,20 +15,56 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        session.beginDialog('collectData', session.dialogData.profile);
+        session.beginDialog('createAlarm', session.dialogData.profile); 
     },
     function (session, results) {
         session.dialogData.profile = results.response;
-        if (session.userData.profile.date.toLowerCase() == 'wednesday') {
+        if (session.dialogData.profile.date.toLowerCase() == 'wednesday') {
             session.send('it is Wednesday my dudes');
         } else {
             session.send('It is not Wednesday my dudes');
         }
-        session.send(`Hello ${session.userData.profile.name}! I love ${session.userData.profile.date}!`);
+        session.send(`Hello ${session.dialogData.profile.name}! I love ${session.dialogData.profile.date}!`);  
+    },
+    function(session, results){
+        session.beginDialog('createAlarm');
+    },
+    function(session, results){
+        session.send(results.response); 
     }
 ]);
 
-bot.dialog
+bot.dialog('createAlarm', [
+    function(session){
+        session.dialogData.alarm = {}; 
+        builder.Prompts.text(session, "What would you like to name this alarm?"); 
+    },
+    function(session, results, next){
+        if(results.response){
+            session.dialogData.name = results.response;
+            builder.Prompts.time(session, "What time would you like to set an alarm for?"); 
+        } else {
+            next(); 
+        }
+
+    },
+    function(session, results){
+        if(results.response){
+            session.dialogData.time = builder.EntityRecognizer.resolveTime([results.response]); 
+        }
+
+        // return alarm to caller
+        if (session.dialogData.name && session.dialogData.time){
+            session.endDialogWithResult({
+                response: { name: session.dialogData.name, time: session.dialogData.time }
+            });
+        } else {
+            session.endDialogWithResult({
+                resumed: builder.ResumeReason.notCompleted
+            });
+        }
+    }
+]);
 
 bot.dialog('collectData', [
     function (session, args, next) {
